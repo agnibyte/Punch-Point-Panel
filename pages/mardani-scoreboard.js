b9b4420316fd51d927b5796f1b0592823c21efea
suraj <agnibyte@gmail.com>
2024-12-14 17:16:41 +0530

    ui chnages

--- pages/mardani-scoreboard.js
+++ pages/mardani-scoreboard.js
@@ -202,7 +202,7 @@ export default function EnhancedScoreboard() {
           width={"75"}
           height={"75"}
         />
-            {/* <Image
+        {/* <Image
             src="/images/trophy.gif"
             alt="Trophy Animation"
             width={80} // Set the width in pixels
@@ -230,46 +230,41 @@ export default function EnhancedScoreboard() {
         </div>
       </header>
 
-{/* Participant Section */}
-{participant && (
-  <div className="flex flex-col md:flex-row items-center justify-between w-full  backdrop-blur-md p-6 r space-y-6 md:space-y-0 md:space-x-8">
-    {/* Trophy Image */}
-    <div className="relative flex items-center justify-center">
-      <Image
-        src="/images/trophy.gif"
-        alt="Trophy Animation"
-        width={100}
-        height={100}
-        className="w-28 h-28 rounded-full  shadow-lg transition-transform duration-300 hover:scale-110 hover:border-yellow-500"
-      />
-      <div className="absolute w-full h-full rounded-full border-2 border-yellow-300 animate-pulse" />
-    </div>
+      {/* Participant Section */}
+      {participant && (
+        <div className="flex flex-col md:flex-row items-center justify-between w-full  backdrop-blur-md p-2 r space-y-6 md:space-y-0 md:space-x-8">
+          {/* Trophy Image */}
+          <div className="relative flex items-center justify-center">
+            <Image
+              src="/images/trophy.gif"
+              alt="Trophy Animation"
+              width={60}
+              height={60}
+              className=" rounded-full  shadow-lg transition-transform duration-300 hover:scale-110 hover:border-yellow-500"
+            />
+            <div className="absolute w-full h-full rounded-full border-2 border-yellow-300 animate-pulse" />
+          </div>
 
-    {/* Participant Details */}
-    <div className="flex flex-col items-center md:items-start">
-      <h2 className="text-3xl font-bold text-center text-gray-100 md:text-left">
-        <span className="">
-          Participant:
-        </span>{" "}
-        {participant}
-      </h2>
-    </div>
+          {/* Participant Details */}
+          <div className="flex flex-col items-center md:items-start">
+            <h2 className="text-3xl font-bold text-center text-gray-100 md:text-left">
+              <span className="">Participant:</span> {participant}
+            </h2>
+          </div>
 
-    {/* Start New Match Button */}
-    <div>
-      <button
-        onClick={() => router.push("/")}
-        className="px-8 py-3 text-lg font-semibold text-gray-900 bg-gradient-to-r from-yellow-400 to-yellow-500 rounded-lg shadow-lg hover:from-yellow-500 hover:to-yellow-600 hover:shadow-yellow-500/50 transition-transform duration-300 transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-yellow-400"
-      >
-        Start New Match
-      </button>
-    </div>
-  </div>
-)}
+          {/* Start New Match Button */}
+          <div>
+            <button
+              onClick={() => router.push("/")}
+              className="px-8 py-3 text-lg font-semibold text-gray-900 bg-gradient-to-r from-yellow-400 to-yellow-500 rounded-lg shadow-lg hover:from-yellow-500 hover:to-yellow-600 hover:shadow-yellow-500/50 transition-transform duration-300 transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-yellow-400"
+            >
+              Start New Match
+            </button>
+          </div>
+        </div>
+      )}
 
-
-
-        {/* // <h2 className="mt-12 text-3xl md:text-4xl font-semibold bg-gradient-to-r from-blue-500 to-blue-400 text-black px-8 py-4 rounded-lg shadow-lg text-center">
+      {/* // <h2 className="mt-12 text-3xl md:text-4xl font-semibold bg-gradient-to-r from-blue-500 to-blue-400 text-black px-8 py-4 rounded-lg shadow-lg text-center">
         //   Participant: {participant}
         // </h2>
       )} */}
@@ -278,7 +273,10 @@ export default function EnhancedScoreboard() {
         {/* Timer Section */}
         <div className="relative bg-gradient-to-tr from-gray-800 to-gray-900 text-white p-12 rounded-2xl shadow-2xl flex flex-col items-center justify-center transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-[0_10px_25px_rgba(0,0,0,0.3)] hover:from-gray-700 hover:to-gray-800">
           <h3 className="text-2xl font-semibold mb-4 flex items-center">
-            <FaStopwatch className="mr-4" size={36} />
+            <FaStopwatch
+              className="mr-4"
+              size={36}
+            />
             {isMatchOver ? "Match Over" : "Time Remaining"}
           </h3>
           <p className="text-8xl font-mono">{formatTime(timer)}</p>
@@ -297,7 +295,7 @@ export default function EnhancedScoreboard() {
           {!matchStarted && !isMatchOver && (
             <button
               onClick={startMatch}
-              className="rounded-full px-8 py-4 text-2xl font-bold bg-blue-600 hover:bg-blue-700 transition duration-200"
+              className="rounded-full px-2 py-4 text-2xl font-bold bg-blue-600 hover:bg-blue-700 transition duration-200"
             >
               Start Match
             </button>
@@ -333,24 +331,13 @@ export default function EnhancedScoreboard() {
 
           {/* Download PDF Button */}
           {isMatchOver && (
-            <div className="flex flex-col gap-4 justify-center items-center">
-              {/* Download PDF Button */}
-              <button
-                onClick={downloadPDF}
-                className="rounded-full px-8 py-4 text-2xl font-bold bg-red-600 hover:bg-pink-500 flex justify-center items-center space-x-2 transition-transform duration-300 ease-in-out hover:scale-105"
-              >
-                <AiOutlineFilePdf size={24} />
-                <span>Download PDF</span>
-              </button>
-
-              {/* Start New Match Button */}
-              <button
-                onClick={() => router.push("/")}
-                className="rounded-full px-8 py-4 text-2xl font-bold bg-green-500 hover:bg-green-600 flex justify-center items-center space-x-2 transition-transform duration-300 ease-in-out hover:scale-105"
-              >
-                Start New Match
-              </button>
-            </div>
+            <button
+              onClick={downloadPDF}
+              className="rounded-full px-8 py-4 text-2xl font-bold bg-red-600 hover:bg-red-700 flex justify-center items-center space-x-2 transition-transform duration-300 ease-in-out "
+            >
+              <AiOutlineFilePdf size={24} />
+              <span>Download PDF</span>
+            </button>
           )}
         </div>
 
@@ -391,7 +378,7 @@ export default function EnhancedScoreboard() {
                 className="mt-8 w-full text-center text-4xl bg-gray-800 text-yellow-400 border border-gray-600 rounded-lg p-4"
               />
             ) : (
-              <p className="mt-8 text-3xl font-bold text-green-400">
+              <p className="mt-8 text-3xl font-bold text-white">
                 Final Score: {score}
               </p>
             )}
